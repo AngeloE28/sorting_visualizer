bf23066f27ec1a629bcc3bc2a25a2d94d01feb44
AngeloE28 <66254064+AngeloE28@users.noreply.github.com>
2022-04-04 18:10:47 +1000

    Refactor, bubble sort and animations
    Refactored some code to remove duplicated code and turned it into a function
    Implemented bubble sort
    Implemented animations for the bubble sort and added extra animations for the sorts

--- src/Sorting_Algorithm/Sorting_Algorithm.js
+++ src/Sorting_Algorithm/Sorting_Algorithm.js
@@ -1,27 +1,112 @@
-export const mergeSort = array => {
-    // Check size of array is valid
-    if(array.length === 1)
-        return array;
+export function mergeSort(arr) {    
+    const anims = [];
+    if(arr.length <= 1)
+        return arr;
+    
+    const auxiliaryArr = arr.slice();
+    mergeSortHelper(arr, 0, arr.length - 1, auxiliaryArr, anims);
+    return anims;
+}
 
-    const middleIdx = Math.floor(array.length/2);
-    const firstHalf = mergeSort(array.slice(0, middleIdx));
-    const secondHalf = mergeSort(array.slice(middleIdx));
-    const sortedArr = [];
+function mergeSortHelper(
+    mainArr,
+    startIdx,
+    endIdx,
+    auxiliaryArr,
+    anims) {
+        if(startIdx === endIdx) 
+            return;
+        
+        const middleIdx = Math.floor((startIdx + endIdx) / 2);
+        mergeSortHelper(auxiliaryArr, startIdx, middleIdx, mainArr, anims);
+        mergeSortHelper(auxiliaryArr, middleIdx + 1, endIdx, mainArr, anims);
+        merge(mainArr, startIdx, middleIdx, endIdx, auxiliaryArr, anims);
+}
 
-    let i = 0, j = 0;
-
-    while( i < firstHalf.length && j < secondHalf.length) {
-        if(firstHalf[i] < secondHalf[i]) 
-            sortedArr.push(firstHalf[i++]);
-        else
-            sortedArr.push(secondHalf[j++]);
+function merge(
+    mainArr, 
+    startIdx,
+    middleIdx,
+    endIdx,
+    auxiliaryArr,
+    anims) {
+    let k = startIdx;
+    let i = startIdx;
+    let j = middleIdx + 1;
+    while(i <= middleIdx && j <= endIdx) {
+        // Values being compared, push them once to change their color
+        anims.push([i, j]);
+        // Values being compared, push them again to revert their color
+        anims.push([i, j]);                        
+        if(auxiliaryArr[i] <= auxiliaryArr[j]) {
+            // Overwrite the value at k in the original array with the value
+            // at i in the aux array
+            anims.push([k, auxiliaryArr[i]]);                
+            mainArr[k++] = auxiliaryArr[i++];
+        } else {
+            // Overwrite the value at k in the original array with the value
+            // at j in the aux array
+            anims.push([k, auxiliaryArr[j]]);
+            mainArr[k++] = auxiliaryArr[j++];
+        }            
     }
+    while(i <= middleIdx) {
+        // Repeat first while loop, compare i to i and overwrite
+        // k with i
+        anims.push([i, i]);            
+        anims.push([i, i]);            
+        anims.push([k, auxiliaryArr[i]]);
+        mainArr[k++] = auxiliaryArr[i++];
+    }
+    while(j <= endIdx) {
+        // Repeat second while loop, instead of value i, its value j
+        anims.push([j, j]);            
+        anims.push([j, j]);     
+        anims.push([k, auxiliaryArr[j]]);            
+        mainArr[k++] = auxiliaryArr[j++];
+    }
+}
 
-    while(i < firstHalf.length) 
-        sortedArr.push(firstHalf[i++]);
-    
-    while(j < secondHalf.length)
-        sortedArr.push(secondHalf[j++])
-    
-    return sortedArr;
-};
+export function bubbleSort(arr) {
+    const anims = [];
+    if(arr.length <= 1)
+        return arr;
+
+    const auxArr = arr.slice();
+    bubble(auxArr, anims);
+    return anims;     
+}
+
+function bubble(mainArr, anims) {
+    const length = mainArr.length;    
+    let swapped;
+    let i, j;
+    for(i = 0; i < length - 1; i++) {
+        swapped = false;
+        for(j = 0; j < length - i - 1; j++) {
+            // Values being compared, push them to change and rever their color
+            anims.push([j, j + 1]);
+            anims.push([j, j + 1]);
+            if(mainArr[j] > mainArr[j + 1]) {            
+                // Values that are swapped                   
+                anims.push([j, mainArr[j + 1]]);
+                anims.push([j + 1, mainArr[j]]);
+                swap(mainArr, j, j+1); 
+                swapped = true;
+            } else {
+                // Values to be ignored for the animation
+                anims.push([-1, -1]);
+                anims.push([-1, -1]);
+            }            
+        }
+        if(!swapped){
+            break;
+        }
+    }
+}
+
+function swap(mainArr, item1, item2) {
+    let temp = mainArr[item1];
+    mainArr[item1] = mainArr[item2];
+    mainArr[item2] = temp;   
+}
--- src/Sorting_Visualizer/Sorting_Visualizer.jsx
+++ src/Sorting_Visualizer/Sorting_Visualizer.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import './Sorting_Visualizer.css';
 import * as Sorting_Algorithms from '../Sorting_Algorithm/Sorting_Algorithm';
 
+const ANIM_SPEED_MS = 5;
+const NUMBER_OF_ARRAY_BARS = 100;
 
 export class Sorting_Visualizer extends React.Component {
     constructor(props) {
         super(props);
-
+        this.val = 0;
         this.state = {
-            array: [],
+            array: [],                        
         };
     }
 
@@ -18,10 +20,15 @@ export class Sorting_Visualizer extends React.Component {
 
     resetArray() {
         const array = [];
-        for (let i = 0; i < 200; i++){
-            array.push(randIntFromInterVals(5, 700));
-        }
+        for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++){
+            array.push(randIntFromInterVals(5, 700));                        
+        }        
         this.setState({array});
+
+        const arrBars = document.getElementsByClassName('array-bar');        
+        for(let i = 0; i < arrBars.length; i++) {            
+            arrBars[i].style.backgroundColor = 'orange';
+        }
     }
 
     testSort() {
@@ -31,15 +38,64 @@ export class Sorting_Visualizer extends React.Component {
             for(let i = 0; i < length; i++) {
                 arr.push(randIntFromInterVals(-1000, 1000));
             }
-            const jsSortedArr = arr.slice().sort((a, b) => a -b);
-            const mergeSortArr = Sorting_Algorithms.mergeSort(array.slice());
+            const jsSortedArr = arr.slice().sort((a, b) => a - b);
+            const mergeSortArr = Sorting_Algorithms.bubbleSort(arr.slice());
             console.log(arraysEqual(jsSortedArr, mergeSortArr));
         }
     }
 
+    colorSort(color, arrBars, barOneIdx, barTwoIdx) {
+        const barOneStyle = arrBars[barOneIdx].style;
+        const barTwoStyle = arrBars[barTwoIdx].style;
+        barOneStyle.backgroundColor = color;
+        barTwoStyle.backgroundColor = color;                
+    }
 
-    mergeSort() {
-        const sortedArr = Sorting_Algorithms.mergeSort(this.state.array);
+    finishedColorSort(arrBars){
+        for(let j = 0; j < arrBars.length; j++) {
+            setTimeout(() => {
+                arrBars[j].style.backgroundColor = 'green';                            
+            }, j * ANIM_SPEED_MS)
+        }     
+    }
+
+    heightAnimSort(arrBars, barIdx, newHeight) {
+        const barStyle = arrBars[barIdx].style;
+        barStyle.height = `${newHeight}px`;         
+    }
+
+    revertAfterFinishedColorSort(arrBars){
+        setTimeout(() => {                               
+            for(let k = 0; k < arrBars.length; k++) {
+                setTimeout(() => {
+                    arrBars[k].style.backgroundColor = 'orange';                    
+                }, k * ANIM_SPEED_MS)
+            }
+        }, 1500);
+    }
+    
+    mergeSort() {        
+        const anims = Sorting_Algorithms.mergeSort(this.state.array);        
+        this.val = 0;        
+        for(let i = 0; i < anims.length; i++) {            
+            const arrBars = document.getElementsByClassName('array-bar');        
+            const isColorChange = i % 3 !== 2;
+            setTimeout(() => {
+                if(isColorChange) {
+                    const color = i % 3 === 0 ? 'red' : 'orange';                
+                    const[barOneIdx, barTwoIdx] = anims[i];                    
+                    this.colorSort(color, arrBars, barOneIdx, barTwoIdx);
+                } else {                
+                    const [barIdx, newHeight] = anims[i];
+                    this.heightAnimSort(arrBars, barIdx, newHeight);
+                }                
+                this.val++;
+                if(this.val === anims.length) {
+                    this.finishedColorSort(arrBars);
+                    this.revertAfterFinishedColorSort(arrBars);
+                }
+            }, i * ANIM_SPEED_MS)
+        }      
     }
 
     quickSort() {
@@ -51,7 +107,31 @@ export class Sorting_Visualizer extends React.Component {
     }
 
     bubbleSort() {
-        
+        const anims = Sorting_Algorithms.bubbleSort(this.state.array);                         
+        this.val = 0;        
+        for(let i = 0; i < anims.length; i++) {            
+            const isColorChange = (i % 4 === 0) || (i % 4 === 1);
+            const arrBars = document.getElementsByClassName('array-bar');            
+            setTimeout(() => {                
+                if(isColorChange) {                                        
+                    this.val++;                                             
+                    const color = i % 4 === 0 ? 'red' : 'orange';                
+                    const [barOneIdx, barTwoIdx] = anims[i];
+                    this.colorSort(color, arrBars, barOneIdx, barTwoIdx);       
+                } else {                    
+                    this.val++;                   
+                    const[barIdx, newHeight] = anims[i];                    
+                    if(barIdx === -1) {     
+                        return;
+                    }
+                    this.heightAnimSort(arrBars, barIdx, newHeight);         
+                }   
+                if(this.val === anims.length || (this.val + 2) === anims.length) {
+                    this.finishedColorSort(arrBars);
+                    this.revertAfterFinishedColorSort(arrBars);
+                }                                
+            }, i * ANIM_SPEED_MS)
+        }
     }
 
 
@@ -85,18 +165,14 @@ function randIntFromInterVals(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function arraysEqual(arr1, arr2) {
-    // Check length is equal
+function arraysEqual(arr1, arr2) {    
     if(arr1.length !== arr2.length)
         return false;
-        
-    // Iterate through arrays making sure each element is equal
-    // Break if elements are not equal
+       
     for(let i = 0; i < arr1.length; i++) {
         if(arr1[i] !== arr2[i])
             return false;
     }
 
-    // Elements are equal
     return true;
 }
